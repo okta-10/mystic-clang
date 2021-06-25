758dc255555cc05f75d8b7577393d83eee71323a
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-06-25 09:13:48 +0700

    aosp-clang: Update to 12.0.6 (based on r416183c)
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- include/clang/Config/config.h
+++ include/clang/Config/config.h
@@ -64,7 +64,7 @@
 #define CLANG_HAVE_RLIMITS 1
 
 /* The LLVM product name and version */
-#define BACKEND_PACKAGE_STRING "LLVM 12.0.5git"
+#define BACKEND_PACKAGE_STRING "LLVM 12.0.6git"
 
 /* Linker version detected at compile time. */
 /* #undef HOST_LINK_VERSION */
--- include/llvm/Config/llvm-config.h
+++ include/llvm/Config/llvm-config.h
@@ -69,10 +69,10 @@
 #define LLVM_VERSION_MINOR 0
 
 /* Patch version of the LLVM API */
-#define LLVM_VERSION_PATCH 5
+#define LLVM_VERSION_PATCH 6
 
 /* LLVM version string */
-#define LLVM_VERSION_STRING "12.0.5git"
+#define LLVM_VERSION_STRING "12.0.6git"
 
 /* Whether LLVM records statistics for use with GetStatistics(),
  * PrintStatistics() or PrintStatisticsJSON()
